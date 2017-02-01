5ec28dd4f2f197e4289b709523df9296edf0d878
Shantanu Laghate <shon615@gmail.com>
2017-01-31 23:04:23 -0500

    Completed Pset 2

---
+++ Linear/src/LinkedList/CircularLinkedList.java
@@ -0,0 +1,41 @@
+package LinkedList;
+
+public class CircularLinkedList<T extends Comparable<T>> {
+	Node<T> tail;
+	
+	public void addToFront (T data) {
+		Node n = new Node(data);
+		if (tail == null) {
+			
+			n.next = tail;
+			tail = n;
+		}
+		else {
+			n.next = tail.next;
+			tail.next = n;
+		}
+	}
+	
+	public void addToRear (T data) {
+		Node n = new Node(data);
+		if (tail == null) {
+			
+			n.next = tail;
+			tail = n;
+		}
+		else {
+			n.next = tail.next;
+			tail.next = n;
+			tail = n;
+		}
+	}
+	
+	public boolean search (T target) {
+		if (tail == null) return false;
+		for (Node n = tail.next; n != tail; n = n.next){
+			if (n.data.equals(target)) return true;
+		}
+		if (tail.data.equals(target)) return true;
+		return false;
+	}
+}
---
+++ Linear/src/LinkedList/MyCoolException.java
@@ -0,0 +1,7 @@
+package LinkedList;
+
+public class MyCoolException extends Exception{
+	public MyCoolException() {
+		super();
+	}
+}
